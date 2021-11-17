6dc31be23d69fff2e8bb2a2d779baeaba3147cf9
vishalgadhvi459 <vishalgadhvi459@gmail.com>
2021-11-17 11:33:15 -0800

    database connected

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -2,7 +2,17 @@ const app = require("./app");
 
 const dotenv = require("dotenv"); 
 
+const connectDatabase = require("./config/database");
+
+
+//config
 dotenv.config({path:"backend/config/config.env"});
+
+
+//connecting to database
+
+connectDatabase();
+
 
 app.listen(process.env.PORT,()=>{
 
