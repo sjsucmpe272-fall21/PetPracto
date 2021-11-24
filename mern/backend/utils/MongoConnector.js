a8f1e6463a66d2a56641b9a9fcc7f162becf8e3c
rohit-sai-chowdary <rohitsaichowdary.potluri@sjsu.edu>
2021-11-24 12:59:26 -0800

    Removed the URI as the directory is public

--- mern/backend/utils/MongoConnector.js
+++ mern/backend/utils/MongoConnector.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
 var options = {
-    maxPoolSize: 100,
-    wtimeoutMS: 2500,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  maxPoolSize: 100,
+  wtimeoutMS: 2500,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-let uri = "mongodb+srv://root123:root123@272mern.eg02c.mongodb.net/272mern?retryWrites=true&w=majority"
+// please enter the uri sent on discord
+let uri = '';
 
-var connection=mongoose.connect(uri, options, (err, res) => {
-    if (err) {
-        console.log(err);
-        console.log(`MongoDB Connection Failed`);
-    } else {
-        console.log(`MongoDB Connected`);
-    }
+var connection = mongoose.connect(uri, options, (err, res) => {
+  if (err) {
+    console.log(err);
+    console.log(`MongoDB Connection Failed`);
+  } else {
+    console.log(`MongoDB Connected`);
+  }
 });
-module.exports=connection
+module.exports = connection;
