0af752d036cc3fc183113d66fb798e4e9c50cdf4
rohit-sai-chowdary <rohitsaichowdary.potluri@sjsu.edu>
2021-11-24 12:45:54 -0800

    Added All models and Mongo connection file

---
+++ mern/backend/utils/MongoConnector.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+var options = {
+    maxPoolSize: 100,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+let uri = "mongodb+srv://root123:root123@272mern.eg02c.mongodb.net/272mern?retryWrites=true&w=majority"
+
+var connection=mongoose.connect(uri, options, (err, res) => {
+    if (err) {
+        console.log(err);
+        console.log(`MongoDB Connection Failed`);
+    } else {
+        console.log(`MongoDB Connected`);
+    }
+});
+module.exports=connection
