c24a9eef6638355020b600b47de1b9e757ec71cf
vishalgadhvi459 <vishalgadhvi459@gmail.com>
2021-11-25 13:36:34 -0800

    Update productRoute.js

--- mern/backend/routes/productRoute.js
+++ mern/backend/routes/productRoute.js
@@ -6,16 +6,17 @@ const { isaAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
+//add this to the roles you want to check as admin or user "isaAuthenticatedUser,authorizeRoles("Admin")"
 
-router.route("/products").get(isaAuthenticatedUser,authorizeRoles("Admin"),getAllProducts);
+router.route("/products").get(getAllProducts);
 
 
-router.route("/product/new").post(isaAuthenticatedUser,createProduct);
+router.route("/product/new").post(isaAuthenticatedUser,authorizeRoles("Admin"),createProduct);
 
 router
 .route("/product/:id")
-.put(isaAuthenticatedUser,updateProduct)
-.delete(isaAuthenticatedUser,deleteProduct)
+.put(isaAuthenticatedUser,authorizeRoles("Admin"),updateProduct)
+.delete(isaAuthenticatedUser,authorizeRoles("Admin"),deleteProduct)
 .get(getProductDetails);
 
 
