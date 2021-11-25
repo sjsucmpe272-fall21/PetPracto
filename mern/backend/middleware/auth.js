514603ed15f75eaf41dab2ea351181a4a1dc2fdb
vishalgadhvi459 <vishalgadhvi459@gmail.com>
2021-11-25 13:00:42 -0800

    admin_role

--- mern/backend/middleware/auth.js
+++ mern/backend/middleware/auth.js
@@ -14,7 +14,24 @@ exports.isaAuthenticatedUser = async(req,res,next)=>{
 
     const decodedData = jwt.verify(token,process.env.JWT_SECRET);
 
-    req.User = await User.findById(decodedData.id);
+    req.user = await User.findById(decodedData.id);
 
     next();
 };
+
+
+//authorize roles
+
+exports.authorizeRoles = (...roles) =>{
+    return (req,res,next)=>{
+
+        if(!roles.includes(req.user.role)){
+            return next(
+            new ErrorHander(`Role: ${req.user.role} is not allowed to access this resource`, 403)
+            );
+        }
+
+        next();
+
+    };
+}
--- mern/backend/routes/productRoute.js
+++ mern/backend/routes/productRoute.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 
 const { getAllProducts,createProduct,updateProduct, deleteProduct, getProductDetails } = require("../controllers/productcontroller");
-const { isaAuthenticatedUser } = require("../middleware/auth");
+const { isaAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 
-router.route("/products").get(getAllProducts);
+router.route("/products").get(isaAuthenticatedUser,authorizeRoles("Admin"),getAllProducts);
 
 
 router.route("/product/new").post(isaAuthenticatedUser,createProduct);
