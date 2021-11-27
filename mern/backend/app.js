776cd60694e71e3f296396d7cd5c43f3c9465e14
rohit-sai-chowdary <rohitsaichowdary.potluri@sjsu.edu>
2021-11-27 10:38:20 -0800

    made few changes. added cors package.

--- mern/backend/app.js
+++ mern/backend/app.js
@@ -1,24 +1,31 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
 
-const app =express();
+const errorMiddleware = require('./middleware/error');
 
-const errorMiddleware = require("./middleware/error");
-
-app.use(express.json())
+const petRouter = require('./routes/petOwnerRoutes');
 
 app.use(cookieParser());
-
-
+app.use(express.json());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors({ origin: `http://localhost:3000`, credentials: true }));
+app.use(cookieParser());
 
 //import route
-const product = require("./routes/productRoute");
-const user = require("./routes/userRoute");
+const product = require('./routes/productRoute');
+const user = require('./routes/userRoute');
+const vet = require('./routes/VetRoutes');
 
-app.use("/api/v1",product);
-app.use("/api/v1",user);
+app.use('/api/v1', product);
+app.use('/api/v1', user);
+app.use('/Pet', petRouter);
+app.use('/Vet', vet);
 
 //middleware for error
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
--- mern/backend/server.js
+++ mern/backend/server.js
@@ -1,21 +1,21 @@
-const app = require("./app");
+const app = require('./app');
 
-const dotenv = require("dotenv"); 
-
-const connectDatabase = require("./config/database");
-
+const dotenv = require('dotenv');
+const mongo = require('./utils/MongoConnector');
+const connectDatabase = require('./config/database');
 
 //config
-dotenv.config({path:"backend/config/config.env"});
-
+dotenv.config({ path: 'backend/config/config.env' });
 
 //connecting to database
+mongo;
+// connectDatabase();
 
-connectDatabase();
+console.log(process.env.PORT);
+app.listen(3030, () => {
+  console.log(`server is working on http://localhost:3030`);
+});
 
-
-app.listen(process.env.PORT,()=>{
-
-    console.log(`server is working on http://localhost:${process.env.PORT}`)
-
-})
+// app.listen(process.env.PORT, () => {
+//   console.log(`server is working on http://localhost:${process.env.PORT}`);
+// });
